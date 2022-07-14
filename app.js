ae327f45c257418f53e1c8ed491448b2a86583f0
EricM76 <menaericdaniel@gmail.com>
2022-07-14 17:53:33 -0300

    estilos y maquetado register listo

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )));
 app.get('/product-detail', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html' )));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html' )));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html' )));
+app.get('/profile', (req,res) => res.sendFile(path.join(__dirname, 'views', 'profile.html' )));
 
 app.listen(port, () => console.log('Server running in http://localhost:' + port))
