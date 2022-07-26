ab1ccf310e5e0750e0c55e5b09e7c73dfb9fb84b
EricM76 <menaericdaniel@gmail.com>
2022-07-26 16:26:30 -0300

    add controllers & routers

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const port = 3030;
-
 const app = express();
-const indexController = require('./controllers/indexController');
+
+const indexRouter = require('./routes/index');
+
+
+
 const productController = require('./controllers/productController');
 const userController = require('./controllers/userController');
 
 app.use(express.static('public'));
 
-app.get('/', indexController.index);
+/* rutas */
+app.use('/',indexRouter);
 app.get('/product-detail', productController.detail);
 app.get('/register', userController.register);
 app.get('/login', userController.login );
