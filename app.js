0391f029427d3dde55bc61fabc9cbe29a079d44c
EricM76 <menaericdaniel@gmail.com>
2022-07-26 17:07:59 -0300

    add routes

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ const port = 3030;
 const app = express();
 
 const indexRouter = require('./routes/index');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
 
-
-
-const productController = require('./controllers/productController');
-const userController = require('./controllers/userController');
 
 app.use(express.static('public'));
 
 /* rutas */
 app.use('/',indexRouter);
-app.get('/product-detail', productController.detail);
-app.get('/register', userController.register);
-app.get('/login', userController.login );
-app.get('/profile', userController.profile);
+app.use('/products', productRouter);
+app.use('/users',userRouter);
 
 app.listen(port, () => console.log('Server running in http://localhost:' + port))
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const indexController = require('../controllers/indexController');
 
-
+/* / */
 router.get('/', indexController.index);
 
 
